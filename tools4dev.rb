2ab95e1b66d708962d2f1c7812a70ffd06393e90
Quentin <Quentin.nio@gmail.com>
2023-09-12 12:45:13 +0200

    Update tools4dev.rb
    update version to dev337

--- tools4dev.rb
+++ tools4dev.rb
@@ -1,9 +1,9 @@
 class Tools4dev < Formula
     desc "UNIX zsh Library (shell as functions)"
     homepage "https://github.com/T4D-Suites/Tools4Dev"
-    url "https://github.com/T4D-Suites/Tools4Dev/releases/download/7.0.0-dev324/tools4dev_7.0.0-dev324.tar"
-    version "7.0.0-dev324"
-    sha256 "2fea764a36e41664c90350f79bcf4708b0233a4657b3a307e5427c83faaf054f"
+    url "https://github.com/T4D-Suites/Tools4Dev/releases/download/7.0.0-dev337/tools4dev_7.0.0-dev337.tar"
+    version "7.0.0-dev337"
+    sha256 "6294a9f821da7e32e559a5a6d0b801c43554b22b4771142cdbe8b6b72eb72adf"
     license "MIT-Modern-Variant"
   
     depends_on "zsh"
