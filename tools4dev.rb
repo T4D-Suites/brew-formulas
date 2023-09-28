3fd4c76cc046d4c737e611a8bf169b4e0031bbf0
Quentin NIO <quentin.nio@simplifield.com>
2023-09-28 12:03:33 +0200

    chore: bump t4d

--- tools4dev.rb
+++ tools4dev.rb
@@ -1,9 +1,9 @@
 class Tools4dev < Formula
     desc "UNIX zsh Library (shell as functions)"
     homepage "https://github.com/T4D-Suites/Tools4Dev"
-    version "7.0.0-dev349"
+    version "7.0.0-dev351"
     url "https://github.com/T4D-Suites/Tools4Dev/releases/download/#{version}/tools4dev_#{version}.tar"
-    sha256 "87cd5c6037a87a113c95e0646a3d607c71e7543babe07d75625840a085c3a41c"
+    sha256 "6203a55e77f6e3bd53417501b3f8288b79da3a9ba7a6a9b35e3db5786bc2d576"
     license "MIT-Modern-Variant"
   
     depends_on "zsh"
