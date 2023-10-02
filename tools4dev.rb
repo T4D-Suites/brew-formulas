748f103f2a79bbcac1b5958708eceb1cd7229fbc
quentin <quentin.nio@simplifield.com>
2023-10-02 10:15:34 +0200

    chore: minor changes in tools4dev.rb

--- tools4dev.rb
+++ tools4dev.rb
@@ -1,9 +1,9 @@
 class Tools4dev < Formula
     desc "UNIX zsh Library (shell as functions)"
     homepage "https://github.com/T4D-Suites/Tools4Dev"
-    version "7.0.0-dev352"
+    version "7.0.0-dev353"
     url "https://github.com/T4D-Suites/Tools4Dev/releases/download/#{version}/tools4dev_#{version}.tar"
-    sha256 "650cf253d25d8d89a354f098466486ec8a2d772c26c14d561dc5fd9e897d7010"
+    sha256 "941b0a00327f5271c808a71e343455f9bf7d1c2b2fdb44f6a8a5f43396ea6563"
     license "MIT-Modern-Variant"
   
     depends_on "zsh"
