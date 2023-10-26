41eaffe9269fdc1ea4f40a6f0b12500f0df8b72f
Quentin NIO <quentin.nio@simplifield.com>
2023-10-26 11:31:04 +0200

    -m Minor Changes in tools4dev.rb

--- tools4dev.rb
+++ tools4dev.rb
@@ -1,7 +1,7 @@
 class Tools4dev < Formula
     desc "UNIX zsh Library (shell as functions)"
     homepage "https://github.com/T4D-Suites/Tools4Dev"
-    version "7.0.0-RC17"
+    version "7.0.0-rc17"
     url "https://github.com/T4D-Suites/Tools4Dev/releases/download/#{version}/tools4dev_#{version}.tar"
     sha256 "3f2066c9cd69223d7fb99ba7213a63df7c0ebfb3c6eee51af7479a4dfa1e3243"
     license "MIT-Modern-Variant"
