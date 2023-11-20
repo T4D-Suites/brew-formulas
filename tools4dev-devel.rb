8675136347cdad45995f8c80f0efd851e05d1202
Quentin NIO <quentin.nio@simplifield.com>
2023-11-20 16:29:37 +0100

    chore: Minor Changes in tools4dev-devel.rb

--- tools4dev-devel.rb
+++ tools4dev-devel.rb
@@ -3,7 +3,7 @@ class Tools4devDevel < Formula
     homepage "https://github.com/T4D-Suites/Tools4Dev"
     url "file:///dev/null"
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    version "devel"
+    version "0.0.1"
     license "MIT-Modern-Variant"
 
     
@@ -13,7 +13,9 @@ class Tools4devDevel < Formula
       # ENV.deparallelize  # if your formula fails when building in parallel
       # Remove unrecognized options if warned by configure
       # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-      system "mv homebrew-formulas/t4d-devel #{bin}"
+      system "mkdir -p #{bin}"
+      system "git clone git@github.com:T4D-Suites/homebrew-tools4dev.git"
+      system "mv homebrew-tools4dev/t4d-devel #{bin}"
     end
 
     test do
