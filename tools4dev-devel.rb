88aa7c87f0adffa0303087ee2610b1b053aa1582
Quentin NIO <quentin.nio@simplifield.com>
2023-11-20 16:27:32 +0100

    chore: Minor Changes in tools4dev-devel.rb

--- tools4dev-devel.rb
+++ tools4dev-devel.rb
@@ -2,7 +2,7 @@ class Tools4devDevel < Formula
     desc "UNIX zsh Library (shell as functions)"
     homepage "https://github.com/T4D-Suites/Tools4Dev"
     url "file:///dev/null"
-    sha256 "devel"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     version "devel"
     license "MIT-Modern-Variant"
 
