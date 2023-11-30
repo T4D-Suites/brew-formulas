45d5836606772857d2a8d45660e475beb64fc9d8
tk463 <tk_368@outlook.com>
2023-11-30 09:51:42 +0000

    feat: handle v7 stable

---
+++ tools4dev@7.1.0.rb
@@ -0,0 +1,28 @@
+class Tools4devAT710 < Formula
+    desc "UNIX zsh Library (shell as functions)"
+    homepage "https://github.com/T4D-Suites/Tools4Dev"
+    version "7.1.0"
+    url "https://github.com/T4D-Suites/Tools4Dev/releases/download/#{version}/tools4dev_#{version}.tar"
+    sha256 "e0ad041f06abfa16f946c59c165664640cb8ef4fc12291aa1505f00c2575de14"
+    license "MIT-Modern-Variant"
+  
+    depends_on "zsh"
+    depends_on "jq" => :recommended
+    depends_on "awscli" => :optional
+  
+    # on_system macos: do
+      depends_on "coreutils"
+    # end
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      system "./src/Platforms/brew-install.sh", "--prefix=#{prefix}"
+    end
+  
+    test do
+      assert_equal "tools4dev", shell_output("#{bin}/t4d 'echo tools4dev'").chomp
+    end
+  end
+  
